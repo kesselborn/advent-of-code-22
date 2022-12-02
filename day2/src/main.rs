a5074df31314beb37bcedea4ee278c388fb63de5
Daniel Bornkessel <github@bornkessel.com>
2022-12-02 23:53:32 +0100

    Simple day2 / part2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -10,17 +10,20 @@ fn main() -> Result<()> {
     let result = reader
         .lines()
         .fold(0, |acc, line| match line.unwrap().as_ref() {
-            "A X" => acc + 3 + 1, // Rock     vs. Rock
-            "B X" => acc + 0 + 1, // Paper    vs. Rock
-            "C X" => acc + 6 + 1, // Scissors vs. Rock
+            // Rock: 1
+            // Paper: 2
+            // Scissors: 3
+            "A X" => acc + 0 + 3, // lose: Rock vs. Scissors
+            "B X" => acc + 0 + 1, // lose: Paper vs. Rock
+            "C X" => acc + 0 + 2, // lose: Scissors vs. Paper
 
-            "A Y" => acc + 6 + 2, // Rock     vs. Paper
-            "B Y" => acc + 3 + 2, // Paper    vs. Paper
-            "C Y" => acc + 0 + 2, // Scissors vs. Paper
+            "A Y" => acc + 3 + 1, // draw: Rock     vs. Rock
+            "B Y" => acc + 3 + 2, // draw: Paper    vs. Paper
+            "C Y" => acc + 3 + 3, // draw: Scissors vs. Scissors
 
-            "A Z" => acc + 0 + 3, // Rock     vs. Scissors
-            "B Z" => acc + 6 + 3, // Paper    vs. Scissors
-            "C Z" => acc + 3 + 3, // Scissors vs. Scissors
+            "A Z" => acc + 6 + 2, // win: Rock     vs. Paper
+            "B Z" => acc + 6 + 3, // win: Paper    vs. Scissors
+            "C Z" => acc + 6 + 1, // win: Scissors vs. Rock
             _ => acc,
         });
 
